4f95cd3fd898655d78d05450a3e13c4778933ab2
Richard Gates <mail@richardkentgates.com>
2023-02-21 03:22:11 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,13 +1,28 @@
 /* Section A */
 
-define('DISALLOW_FILE_EDIT', TRUE );
+define('WP_MEMORY_LIMIT', **** );
 
 /* Section B */
 
-define('WP_MEMORY_LIMIT', **** );
+define('WP_AUTO_UPDATE_CORE', TRUE );
 
 /* Section C */
+
+define('FS_METHOD', '****' );
 
 define('FS_CHMOD_DIR', **** );
 
 define('FS_CHMOD_FILE', **** );
+
+/* Section D */
+
+define('DISALLOW_FILE_EDIT', TRUE );
+
+/* Section E */
+
+define('FORCE_SSL_ADMIN', TRUE );
+define('FORCE_SSL_LOGIN', TRUE );
+
+/* Section F */
+
+define('WP_POST_REVISIONS', 3);
