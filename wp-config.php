92365789166d15f7a1397fc36c5f96142249aba0
Richard Gates <mail@richardkentgates.com>
2024-01-19 03:08:46 -0600

    Update wp-config.php
    Move core update info to gcm plugin

--- wp-config.php
+++ wp-config.php
@@ -4,25 +4,21 @@ define('WP_MEMORY_LIMIT', **** );
 
 /* Section B */
 
-define('WP_AUTO_UPDATE_CORE', TRUE );
-
-/* Section C */
-
 define('FS_METHOD', '****' );
 
 define('FS_CHMOD_DIR', **** );
 
 define('FS_CHMOD_FILE', **** );
 
-/* Section D */
+/* Section C */
 
 define('DISALLOW_FILE_EDIT', TRUE );
 
-/* Section E */
+/* Section D */
 
 define('FORCE_SSL_ADMIN', TRUE );
 define('FORCE_SSL_LOGIN', TRUE );
 
-/* Section F */
+/* Section E */
 
 define('WP_POST_REVISIONS', 3);
