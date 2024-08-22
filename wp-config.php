49c1c49850b92ad8ba6864c9d91e9d90c6d2e346
Richard Gates <mail@richardkentgates.com>
2024-08-22 17:04:43 -0500

    Update wp-config.php
    Reduced to essential resource restrictions disclosure

--- wp-config.php
+++ wp-config.php
@@ -1,24 +1,8 @@
 /* Section A */
 
-define('WP_MEMORY_LIMIT', **** );
+define('WP_MEMORY_LIMIT', '96M');
+define('WP_MAX_MEMORY_LIMIT', '128M');
 
 /* Section B */
-
-define('FS_METHOD', '****' );
-
-define('FS_CHMOD_DIR', **** );
-
-define('FS_CHMOD_FILE', **** );
-
-/* Section C */
-
-define('DISALLOW_FILE_EDIT', TRUE );
-
-/* Section D */
-
-define('FORCE_SSL_ADMIN', TRUE );
-define('FORCE_SSL_LOGIN', TRUE );
-
-/* Section E */
 
 define('WP_POST_REVISIONS', 3);
